cd10be25acc185ade6709c7a11158c22341c5ba0
Joseph McArthur <joe@righttoresearch.org>
2017-08-21 22:58:44 +0100

    adding some todos

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
+# todo
+# adding varied responses
+# adding some proper copy
+# add in quing
+# move onto the live server
+# add a time delay & ask for feedback
+# see if we can port this to other places
+
 import os
 import sys
 import json
