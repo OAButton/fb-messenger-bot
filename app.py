ed048ff1a30ba12b2666e92e03251dd3630c7260
Joseph McArthur <joe@righttoresearch.org>
2018-04-21 12:20:38 +0100

    turning off debug stuff

--- app.py
+++ app.py
@@ -141,8 +141,10 @@ def send_message(recipient_id, message_text):
 
     myuuid = uuid.uuid4().hex
 
-    log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text + " debug: " + myuuid))
+    log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
+    # log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text + " debug: " + myuuid))
 
+    
     params = {
         "access_token": os.environ["PAGE_ACCESS_TOKEN"]
     }
@@ -154,8 +156,11 @@ def send_message(recipient_id, message_text):
             "id": recipient_id
         },
         "message": {
-            "text": message_text + " debug: " + myuuid
+            "text": message_text
         }
+        #"message": {
+        #    "text": message_text + " debug: " + myuuid
+        #}
     })
     r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
     if r.status_code != 200:
